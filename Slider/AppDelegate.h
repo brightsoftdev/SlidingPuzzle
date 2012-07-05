5921959eeba78b53fa440cc3f8c04b6f23ce6f7a
Chris Sinchok <csinchok@theonion.com>
2012-07-05 12:08:18 -0400

    Initial Commit

---
+++ Slider/AppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.h
+//  Slider
+//
+//  Created by Chris Sinchok on 7/5/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class ViewController;
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) ViewController *viewController;
+
+@end
---
+++ Slider/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Slider
+//
+//  Created by Chris Sinchok on 7/5/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
